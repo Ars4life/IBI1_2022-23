37c35cb016cd724bd7f8d614db32086957f9b21e
Frank <yifan.22@intl.zju.edu.cn>
2023-04-03 16:29:48 +0800

    improve_the_work_of_Practical6

--- Practical6/Favourite_movie_genres.py
+++ Practical6/Favourite_movie_genres.py
@@ -1,16 +1,21 @@
 # create a dictionary variable: favourite_movie = {}
 # input the data in the table into the dictionary
 # print the dictionary
-# make a pie plot with the fucntion pyplot in matplotlib
+# make a pie plot with the function pyplot in matplotlib.
 
-
-#  create a dictionary that contains the data
+# import the 'pyplot' from # import the 'matplotlib'
+from matplotlib import pyplot as plot
+#  create a dictionary that contains the data, the keys are the types of movie genre
+#  and the values are the number of students who favourites.
 favourite_movie = {'Comedy': 73, 'Action': 42, 'Romance': 38,
                    "Fantasy": 28, "Science-fiction": 22, "Horror": 19,
                    "Crime": 18, "Documentary": 12, 'History': 8, 'War': 7}
 print(favourite_movie)
-# import the 'pyplot' from # import the 'matplotlib'
-from matplotlib import pyplot as plot
-# use the dictionary to make a piechart
-plot.pie(list(favourite_movie.values()), labels=list(list(favourite_movie.keys())))
+# use the dictionary to make a piechart, define the label, the sizes, and the plots title
+types_label = list(list(favourite_movie.keys()))
+numbers = list(favourite_movie.values())
+plot.pie(numbers, labels=types_label, autopct='%1')
+plot.axis('equal')
+plot.title('favourite Movie')
 plot.show()
+
--- Practical6/Olympic_Costs.py
+++ Practical6/Olympic_Costs.py
@@ -2,13 +2,21 @@
 # sort the list
 # make a barchart with the pyplot
 
+# import the pyplot
+from matplotlib import pyplot as plot
+import numpy as np
 #  create a list that contains the data
 costs = [1, 8, 15, 7, 5, 14, 43, 40]
 # sort it and print it
 cost_sorted = sorted(costs)
 print(cost_sorted)
-# import the pyplot and make a barchart
-from matplotlib import pyplot as plot
-for i in range(len(cost_sorted)):
-    plot.bar(i, cost_sorted[i])
+# make a barchart with the data
+N = len(cost_sorted)
+width = 0.35
+top_cost = cost_sorted[N-1]
+ind = np.arange(N)
+p1 = plot.bar(ind, cost_sorted, width)
+plot.ylabel('cost')
+plot.yticks(np.arange(0, top_cost, 5))
+plot.title('the coast range of Olympics')
 plot.show()
