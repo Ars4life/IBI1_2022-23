5d6eb050a7fa16f4e6f3305b1bbb38bc54ffb5c2
Frank <yifan.22@intl.zju.edu.cn>
2023-04-13 16:22:22 +0800

    correctify_some_mistakes

--- Practical9/count_cds_genes.py
+++ Practical9/count_cds_genes.py
@@ -1,21 +1,28 @@
 import re
 import os
 
-
-end_codon = input('choose one end codon(TGA, TAA, TAG):')
-
+# read the file and split the string into each sequence
 os.chdir('C:/cygwin64/home/10755/IBI1_2022-23/Practical9/')
 sequence = open(r'Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa', 'r')
 seq_str = sequence.read()
-gene_split = re.split('>',seq_str)
-gene_with_end_conda = []
-for i in gene_split:
-    m = re.sub(r'\n', '', i)
-    if re.search(end_codon+'$', m):
-        h = re.sub(r' .+]', '\n', m)
-        gene_with_end_conda.append('>'+h)
-count = len(gene_with_end_conda)
-output_file = open(end_codon+'_genes.fa', 'w')
-output_file.write('There are '+str(count)+' genes end with \''+end_codon+'\'.\n')
-for i in gene_with_end_conda:
-    output_file.write(i+'\n')
+gene_split = re.split('>', seq_str)
+# make a function to count the input end condon
+def count_the_end(end_codon):
+    gene_with_end_conda = [] # make an empty list to restore the required seq
+    for genes in gene_split:
+        genes_treated_1 = re.sub(r'\n', '', genes) # remove change line
+        if re.search(end_codon, genes_treated_1): # check if there is an wanted end conda
+            genes_treated_2 = re.sub(r' .+]', '\n', genes_treated_1)
+            end_counter = len(re.findall(end_codon, genes_treated_2)) # count the end conda
+            genes_treated_3 = re.sub(r'\n', '('+str(end_counter)+')'+'\n', genes_treated_2)
+            gene_with_end_conda.append('>'+genes_treated_3) # use the list to countian the wanted seq
+    output_file = open(end_codon+'_genes.fa', 'w')
+    for i in gene_with_end_conda:
+        output_file.write(i+'\n')
+
+
+input_end_codon = input('choose one end codon(TGA, TAA, TAG):')
+if input_end_codon in ['TAA', 'TAG', 'TGA']: # check the input
+    count_the_end(input_end_codon)
+else:
+    print('wrong input')
